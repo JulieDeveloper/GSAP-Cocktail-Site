9635332834657c75a8a8bb250f12274444d93b67
Julie Chou <chouyuan424@gmail.com>
2026-02-18 15:51:35 -0500

    Add Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import gsap from 'gsap';
+import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+
+import Navbar from "./components/Navigation.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
- return (
-	<div className="flex-center h-[100vh]">
-        <h1 className="text-3xl text-indigo-300">Hello, GSAP!</h1>
-    </div>
- )
-}
+	return (
+		<main>
+			<Navbar />
+			<div className='h-dvh bg-white'></div>
+		</main>
+	);
+};
 
-export default App
+export default App;
